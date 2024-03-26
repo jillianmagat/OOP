1e5151fe270ca29b771020336e591e9cffdbecb4
jillianmagat <165018287+jillianmagat@users.noreply.github.com>
2024-03-26 21:56:06 +1100

    Update Ex5.java

--- week2/Ex5.java
+++ week2/Ex5.java
@@ -1,20 +1,15 @@
 package week2;
-
 public class Ex5 {
-
 	public static void main(String[] args) {
 		double earthRadius = 7600/2; //radius = diameter/2
-		double sunRadius = 865000/2; //radius = diameter/2
-		
+		double sunRadius = 865000/2; //radius = diameter/2		
 		double earthVolume = (4.0/3.0)*Math.PI*Math.pow(earthRadius, 3);
 		double sunVolume = (4.0/3.0)*Math.PI*Math.pow(sunRadius, 3);
 		double sunRatio = sunVolume/earthVolume;
-
 		System.out.println("The volume of the Earth is " + earthVolume + " cubic miles");
 		System.out.println("The volume of the Sun is " + sunVolume + " cubic miles");
 		System.out.println("The ratio of the Sun to the volume of the Earth is " + sunRatio);
 	}
-
 }
 
 /* Exercise 1.5	The diameter of the Sun is approximately 865,000 miles. The diameter of the earth is approximately 7,600 miles.
