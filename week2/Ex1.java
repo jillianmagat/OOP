508ed3c00abe8ba4516d5bf5109ee285d68bd44f
jillianmagat <165018287+jillianmagat@users.noreply.github.com>
2024-03-26 21:50:13 +1100

    Update Ex1.java

--- week2/Ex1.java
+++ week2/Ex1.java
@@ -1,21 +1,17 @@
 package week2;
-
 import java.util.Scanner;
-
 public class Ex1 { /*subtraction*/
 
 	public static void main(String[] args) {
 		Scanner differenceOfNumbers = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int firstNumber = differenceOfNumbers.nextInt();
-        System.out.print("Enter second number: ");
-        int secondNumber = differenceOfNumbers.nextInt();
-        int difference = firstNumber - secondNumber;
-
-        System.out.println("The difference between " + firstNumber + " and " + secondNumber + " is " 
-        + difference);
+	        System.out.print("Enter first number: ");
+	        int firstNumber = differenceOfNumbers.nextInt();
+	        System.out.print("Enter second number: ");
+	        int secondNumber = differenceOfNumbers.nextInt();
+	        int difference = firstNumber - secondNumber;
+	
+	        System.out.println("The difference between " + firstNumber + " and " + secondNumber + " is " + difference);
 	}
-
 }
 
 /*Exercise 1.1	Write a java program to display the result of difference of two numbers. 
