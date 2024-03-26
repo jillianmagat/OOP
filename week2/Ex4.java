a734108ed11c06a758155da621664ac52c1594e8
jillianmagat <165018287+jillianmagat@users.noreply.github.com>
2024-03-26 21:55:38 +1100

    Update Ex4.java

--- week2/Ex4.java
+++ week2/Ex4.java
@@ -1,24 +1,20 @@
 package week2;
-
 public class Ex4 {
-
 	public static void main(String[] args) {
-	double principal = 5000;
-	double rate = 0.04; //4%
-	double period = 10; //10 years
-	double rateRaisedToPeriod = Math.pow((1+rate), period);
-
-	double compoundInterest = principal*rateRaisedToPeriod;
-	
-	System.out.print("The amount if the principal is $" + principal + " with interest rate " + 
-	rate + " in " + period + " years is \n$");
-	System.out.printf("%.2f", compoundInterest);
+		double principal = 5000;
+		double rate = 0.04; //4%
+		double period = 10; //10 years
+		double rateRaisedToPeriod = Math.pow((1+rate), period);	
+		double compoundInterest = principal*rateRaisedToPeriod;
+		
+		System.out.print("The amount if the principal is $" + principal + " with interest rate " + 
+		rate + " in " + period + " years is \n$");
+		System.out.printf("%.2f", compoundInterest);
 	}
-
 }
 
 /*Exercise 1.4	Compound Interest. Using the compound Interest Formula
-A = P * (1 + r)n 
+A = P * (1 + r)^n 
 where A = amount, r = interest rate per time period, and n = number of time periods.
 Write a Java program to calculate and display the result where 
 P= $ 5000.00 investment ; r = 4% interest rate per year ; n = 10 years.
