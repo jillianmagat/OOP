cfa491792ae021cae714aaefbe1fa64999fbd5dc
jillianmagat <165018287+jillianmagat@users.noreply.github.com>
2024-03-26 21:50:44 +1100

    Update Ex2.java

--- week2/Ex2.java
+++ week2/Ex2.java
@@ -1,14 +1,11 @@
 package week2;
-
 public class Ex2 {
 
 	public static void main(String[] args) {
 		double celsius = 15;
-        double fahrenheit = celsius*9/5 + 32;
-
-        System.out.println(celsius + " celsius to fahrenheit is " + fahrenheit);
+	        double fahrenheit = celsius*9/5 + 32;
+	        System.out.println(celsius + " celsius to fahrenheit is " + fahrenheit);
 	}
-
 }
 
 /* Exercise 1.2	Write a Java program covered in class to convert degrees Celsius to degrees Fahrenheit.
