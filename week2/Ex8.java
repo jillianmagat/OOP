e1dcc9c49ed7873a53e99b52ecdf8117597c2bb8
jillianmagat <165018287+jillianmagat@users.noreply.github.com>
2024-03-26 21:59:18 +1100

    Update Ex8.java

--- week2/Ex8.java
+++ week2/Ex8.java
@@ -1,36 +1,32 @@
 package week2;
-
 import java.util.Scanner;
-
 public class Ex8 {
 	public static void main(String[] args) {
-	Scanner compareStrings = new Scanner(System.in);
-	System.out.println("This program compares two words which you are about to enter");
-	System.out.print("Enter first word: ");
-    String word1 = compareStrings.next();
-    System.out.print("Enter second word: ");
-    String word2 = compareStrings.next();
+		Scanner compareStrings = new Scanner(System.in);
+		System.out.println("This program compares two words which you are about to enter");
+		System.out.print("Enter first word: ");
+		String word1 = compareStrings.next();
+		System.out.print("Enter second word: ");
+		String word2 = compareStrings.next();
     
-    //COMPARE EQUALITY OF TWO STRINGS
-    if (word1.equals(word2)) {
-    	System.out.println("The two given strings are equal");
-    }
+		//COMPARE EQUALITY OF TWO STRINGS
+		if (word1.equals(word2)) {
+			System.out.println("The two given strings are equal");
+		}
+		else {
+			System.out.println("The two given strings are unequal");
+		}
     
-    else {
-    	System.out.println("The two given strings are unequal");
-    }
-    
-    //COMPARE EQUALITY OF THE FIRST LETTERS OF TWO STRINGS
-    char firstLetterWord1 = word1.charAt(0);
-    char firstLetterWord2 = word2.charAt(0);
+		//COMPARE EQUALITY OF THE FIRST LETTERS OF TWO STRINGS
+		char firstLetterWord1 = word1.charAt(0);
+		char firstLetterWord2 = word2.charAt(0);
 
-    if (firstLetterWord1 == firstLetterWord2) {
-    	System.out.println("The first letters of two given strings are equal");
-    }
-    
-    else {
-    	System.out.println("The first letters of two given strings are not equal");
-    }
+		if (firstLetterWord1 == firstLetterWord2) {
+			System.out.println("The first letters of two given strings are equal");
+		}
+		else {
+			System.out.println("The first letters of two given strings are not equal");
+		}
 	}
 }
 
